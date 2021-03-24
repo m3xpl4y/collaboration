7dd6166518b81def0a5055675cbcdc236c76f6fe
dominik <d.braendle@aub.at>
2021-03-24 19:40:31 +0100

    add viergewinnt

---
+++ src/com/company/project/viergewinnt/mainClass.java
@@ -0,0 +1,41 @@
+package com.company.project.viergewinnt;
+
+
+import java.util.Scanner;
+
+    public class mainClass {
+
+
+
+
+
+
+        public static int playstone() {
+
+            int inputstone = 0;
+
+            boolean isValideNumber = true;
+
+            while (isValideNumber) {
+
+                Scanner scan = new Scanner(System.in);
+                inputstone = scan.nextInt();
+
+                if (inputstone >= 1 || inputstone <=7) {
+
+                    inputstone--;
+
+                    isValideNumber = false;
+
+                    return inputstone;
+                }
+
+
+            }
+
+            return inputstone;
+        }
+
+
+    }
+
