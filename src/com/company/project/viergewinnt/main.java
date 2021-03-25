9e3347d2724b70573a4b179a36c190da8fbbc9a4
dominik <d.braendle@aub.at>
2021-03-25 19:10:39 +0100

    update project that put the playstone on the right position with check the player and empty collums

--- src/com/company/project/viergewinnt/main.java
+++ src/com/company/project/viergewinnt/main.java
@@ -1,60 +1,57 @@
 package com.company.project.viergewinnt;
 
-import com.company.project.viergewinnt.mainClass;
 
 public class main {
 
-        static int hight = 6;
-        static int lenght = 7;
-        static char[][] field4IsWin = new char[hight][lenght];
+    static int hight = 6;
+    static int lenght = 7;
+    static char[][] field4IsWin = new char[hight][lenght];
 
 
-        static char player1 = 'X';
-        static char player2 = '0';
+    static char player1 = 'X';
+    static char player2 = '0';
+    static byte player = 0;
 
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
 
-            boolean isPlayEnd = true;
+        boolean isPlayEnd = true;
+
+        mainClass.fielAllRows();
 
 
+        while (isPlayEnd) {
+
+            System.out.println("Bitte geben geben Sie die Spalte ein: 1-7. ");
 
 
+            //System.out.println(mainClass.playstone());
 
+            //Gibt das 4 Gewinnt GUI aus.
 
-            while (isPlayEnd) {
-
-                System.out.println("Bitte geben geben Sie die Spalte ein: 1-7. ");
-
-
-                //System.out.println(mainClass.playstone());
-
-                //Gibt das 4 Gewinnt GUI aus.
-                outputResult(mainClass.fillRows(player1));
-
-
-
-
-
-            }
-
-
+            mainClass.changPlayer(player);
 
 
         }
 
-        private static void outputResult(char[][] field) {
-            for (int i = hight-1; i >= 0; i--) {
+
+    }
+
+
+    public static void outputResult(char[][] field) {
+        for (int i = hight - 1; i >= 0; i--) {
+            System.out.print("|");
+
+            for (int j = 0; j <= lenght - 1; j++) {
+                System.out.print(field[i][j]);
                 System.out.print("|");
 
-                for (int j = 0; j <= lenght-1; j++) {
-                    System.out.print(field[i][j]);
-                    System.out.print("|");
-
-                }
-                System.out.println("");
-
             }
+            System.out.println("");
+
         }
+    }
+
+
 }
--- src/com/company/project/viergewinnt/mainClass.java
+++ src/com/company/project/viergewinnt/mainClass.java
@@ -2,10 +2,14 @@ package com.company.project.viergewinnt;
 
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class mainClass {
 
 static int k [] = new int[main.lenght];
+
+
+        //input form the player to check in with collum the playstone will be entered and decrase it by one for the right index 1 = 0.
 
     public static int playstone() {
 
@@ -34,9 +38,12 @@ static int k [] = new int[main.lenght];
 
         return inputstone;
     }
-
+        //Fill the Array with the given playstone and check if no other is on this place.
 
     public static char[][] fillRows(char player) {
+
+
+        anyCollumISFull();
 
 
         for (int j = 0; j < main.hight; j++) {
@@ -45,23 +52,35 @@ static int k [] = new int[main.lenght];
             System.out.println("Ausgabe Fill Rows: " + i);
 
 
+
             if (main.field4IsWin[j][i] == player) {
                 System.out.println("Schon vorhanden.");
 
-                k []
+                k[i] += 1;
 
+                main.field4IsWin[j+k[i]][i] = player;
 
-                main.field4IsWin[j+k][i] = player;
-                k++;
 
 
                 break;
 
             }
-            if (main.field4IsWin[j][i] != player) {
+            if (main.field4IsWin[j][i] == ' ') {
+
+                System.out.println("ist Leer");
                 main.field4IsWin[j][i] = player;
                 break;
             }
+
+
+            if ( main.field4IsWin[j][i] != player) {
+
+                System.out.println("ist nicht der Spieler");
+                k[i] += 1;
+                main.field4IsWin[j+k[i]][i] = player;
+                break;
+            }
+
 
 
         }
@@ -69,6 +88,60 @@ static int k [] = new int[main.lenght];
         return main.field4IsWin;
 
 
+
+
+
+
+    }
+        //Change the Player after every input of the other player.
+
+    public static byte changPlayer(byte player) {
+        if (player == 0 ) {
+            System.out.println("Der Spieler 1 (X) ist dran.");
+            main.outputResult(mainClass.fillRows('X'));
+
+
+            player = 1;
+
+        }
+        if (player == 1) {
+            System.out.println("Der Spieler 2 (0) ist dran.");
+            main.outputResult(mainClass.fillRows('0'));
+
+            player = 0;
+
+        }
+
+        return  player;
+    }
+
+
+    // Fiell the games GUI with the given playstones.
+
+    public static void fielAllRows(){
+
+        for (int i = 0; i < main.hight; i++) {
+
+            for (int j = 0; j < main.lenght ; j++) {
+
+
+                main.field4IsWin[i][j] = ' ';
+
+            }
+
+
+
+        }
+
+    }
+
+    public static void anyCollumISFull(){
+
+        //check if any Collum is filled with 6 playstones.
+
+        if (IntStream.of(k).anyMatch(x -> x == 5)) {
+            System.out.println("Spiel vorbei");
+        }
     }
 
 
